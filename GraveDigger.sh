c0347aa2b230313b9b1e8b3de0aa67b9aa2654d5
Gilles Biagomba <gbiago961@gmail.com>
2018-09-20 17:54:26 -0400

    Updated README

---
+++ GraveDigger.sh
@@ -0,0 +1,74 @@
+# Author: Gilles Biagomba
+# Program: GraveDigger.sh
+# Description: This script was designed to help you find files, mass copy files and zip them.\n
+
+# Setting working directory
+pth=$(pwd)
+
+# Prompting user for questions
+echo "What filetype are you trying to ind?"
+read FTYPE
+echo
+
+echo "What is keyword you would like to use to filter down your search?"
+echo "If you have none, just press ENTER"
+read FILTER
+echo
+
+echo "What is the name of the zip (output) file you would like to use?"
+read ZFILE
+echo
+
+echo "What is the password for the zip file?"
+read ZPSS
+echo
+
+# Checking to see if variables are empty
+if [ -z $FTYPE ]; then
+	echo "You did not enter a filetype to search, please try again"
+	exit
+elif [ -z $ZFILE ]; then
+	echo "You did not enter a name for the zip file output, please try again"
+	exit
+elif [ -z $ZPSS ]; then
+	echo "You did not enter a password for the zip file output, please try again"
+	exit
+else
+	echo
+	echo "We are good to go!"
+	echo
+fi
+
+# Updating the file databse
+echo "Seat tight, I am updating you file database (i.e., updatedb)"
+updatedb
+
+# Locating the fie(s) in question
+n=0
+echo "Locating your files....hang in there, we are almost done"
+if [ -z $FILTER ]; then
+	FILES=($(locate *.$FTYPE | grep $FTYPE))
+	${$FILES[n++]}
+else
+	FILES=($(locate *.$FTYPE | grep $FTYPE | grep -i $FILTER))
+	${$FILES[n++]}
+fi
+
+# Zipping up said files
+touch $FTYPE-FILE_MANIFESTO.txt
+echo "See patience pays off! Compressing your files now!"
+for FILE in ${FILES[*]}; do
+	echo "Compressing $FILE"
+	zip --password $ZPSS -ru -9 $pth/$ZFILE.zip $FILE | tee -a $FTYPE-FILE_MANIFESTO.txt
+done
+
+# CLeaning up
+unset pth
+unset FTYPE
+unset FILTER
+unset ZFILE
+unset FILES
+unset FILE
+unset n
+unset ZPSS
+set -u
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# GraveDigger
+![alt tag](https://s3.amazonaws.com/ns.images/newspring/collection/series_fuse/GraveDiggers.Rectangle.jpg)
+
+```
+  ________                           ________  .__                            
+ /  _____/___________ ___  __ ____   \______ \ |__| ____   ____   ___________ 
+/   \  __\_  __ \__  \\  \/ // __ \   |    |  \|  |/ ___\ / ___\_/ __ \_  __ \
+\    \_\  \  | \// __ \\   /\  ___/   |    `   \  / /_/  > /_/  >  ___/|  | \/
+ \______  /__|  (____  /\_/  \___  > /_______  /__\___  /\___  / \___  >__|   
+        \/           \/          \/          \/  /_____//_____/      \/       
+```
+
+# Grave Digger
 GraveDigger will search a linux system for files of interest, then compress and encrypt said files for exfiltration. 
+
+# Usage
+./GraveDigger.sh
